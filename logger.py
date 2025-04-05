90e9fea3e0671278d1ae6991b53027cf19e635d0
bryntje <63344707+bryntje@users.noreply.github.com>
2025-04-06 00:34:42 +0200

    Add logging info to logger.py for gptstatus

--- logger.py
+++ logger.py
@@ -10,4 +10,22 @@ logging.basicConfig(
     handlers=[log_handler, logging.StreamHandler()]  # Log to file and console
 )
 
+from datetime import datetime
+
+class GPTStatusLogs:
+    def __init__(self):
+        self.last_success_time = datetime.utcnow()
+        self.last_error_type = None
+        self.average_latency_ms = 420
+        self.total_tokens_today = 3242
+        self.rate_limit_reset = "~12 min"
+        self.current_model = "gpt-3.5-turbo"
+        self.last_user = 123456789012345678  # Discord user ID
+        self.success_count = 8
+        self.error_count = 3
+
+def get_gpt_status_logs():
+    return GPTStatusLogs()
+
+
 logger = logging.getLogger("bot")
