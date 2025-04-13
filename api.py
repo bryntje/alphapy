f57ecceb498a36b5bc99fb15d4b49074ca00b076
bryntje <63344707+bryntje@users.noreply.github.com>
2025-04-13 02:28:46 +0200

    import fix

--- api.py
+++ api.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 import time
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # of specifieker: ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 
 startup_time = time.time()
 
