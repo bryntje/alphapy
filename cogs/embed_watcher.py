2d854a14254b525e49183525059ebaeba183253a
bryntje <63344707+bryntje@users.noreply.github.com>
2025-04-25 00:53:36 +0200

    we tamed it!

--- cogs/embed_watcher.py
+++ cogs/embed_watcher.py
@@ -35,7 +35,7 @@ class EmbedReminderWatcher(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         print("[🐍] Message ontvangen:", message.content)
-        ANNOUNCEMENTS_CHANNEL_ID = 1160511692824924216  # <-- pas aan!
+        ANNOUNCEMENTS_CHANNEL_ID = 1336038676727206030  # <-- pas aan!
 
         if message.channel.id != ANNOUNCEMENTS_CHANNEL_ID or not message.embeds:
             print("[📣] Kanaal ID:", message.channel.id)
